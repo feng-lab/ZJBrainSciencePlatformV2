be05a33590dca03ef3f7a6b8f12627d2f03736f2
蔡涛 <caitao@zhejianglab.com>
2022-11-11 11:21:23 +0800

    添加SubjectHuman表

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, BigInteger, Boolean, func, TIMESTAMP, Text
+from sqlalchemy import Column, String, BigInteger, Boolean, func, TIMESTAMP, Text, Date
 from sqlalchemy.sql import expression
 
 from app.database import Base
@@ -75,7 +75,7 @@ class StaffMaster(Base):
         "creator_id", String(length=255), nullable=False, comment="创建人编号，若为自己创建，则为0"
     )
     email = Column("email", String(length=255), comment="联系邮箱")
-    cellphone_number = Column("cellphone_number", String(length=255), comment="手机号码")
+    cellphone_number = Column("cellphone_number", String(length=50), comment="手机号码")
     gmt_create = Column(
         "gmt_create",
         TIMESTAMP,
@@ -150,5 +150,62 @@ class Message(Base):
         return (
             f"Message(id={self.id!r},sender_id={self.sender_id!r},receiver_id={self.receiver_id!r},"
             f"msg_type={self.msg_type!r},msg_status={self.msg_status!r},content={self.content!r},"
+            f"gmt_create={self.gmt_create!r},gmt_modified={self.gmt_modified!r},is_deleted={self.is_deleted!r})"
+        )
+
+
+class SubjectHuman(Base):
+    __tablename__ = "subject_human"
+
+    id = Column("id", BigInteger, primary_key=True, comment="主键")
+    subject_id = Column(
+        "subject_id",
+        String(length=255),
+        nullable=False,
+        unique=True,
+        index=True,
+        comment="实验对象的唯一识别编号，系统自动生成",
+    )
+    gender = Column("gender", String(length=20), nullable=False, comment="生理性别")
+    birthdate = Column("birthdate", Date, comment="出生日期")
+    education = Column("education", String(length=50), comment="受教育水平")
+    occupation = Column("occupation", String(length=50), comment="职业")
+    marriage_status = Column("marriage_status", String(length=20), comment="婚姻状态")
+    abo_blood_type = Column("abo_blood_type", String(length=20), comment="ABO血型")
+    left_hand_flag = Column("left_hand_flag", String(length=20), comment="是否为左撇子")
+    death_date = Column("death_date", Date, comment="死亡日期")
+    cellphone_number = Column("cellphone_number", String(length=50), comment="手机号码")
+    email = Column("email", String(length=255), comment="联系邮箱")
+    address = Column("address", String(length=255), comment="住址")
+    gmt_create = Column(
+        "gmt_create",
+        TIMESTAMP,
+        nullable=False,
+        server_default=func.utcnow(),
+        comment="创建时间(UTC)",
+    )
+    gmt_modified = Column(
+        "gmt_modified",
+        TIMESTAMP,
+        nullable=False,
+        server_default=func.utcnow(),
+        onupdate=func.utcnow(),
+        comment="最近修改时间(UTC)",
+    )
+    is_deleted = Column(
+        "is_deleted",
+        Boolean,
+        nullable=False,
+        server_default=expression.false(),
+        comment="记录是否被删除",
+    )
+
+    def __repr__(self):
+        return (
+            f"SubjectHuman(id={self.id!r},subject_id={self.subject_id!r},gender={self.gender!r},"
+            f"birthdate={self.birthdate!r},education={self.education!r},occupation={self.occupation!r},"
+            f"marriage_status={self.marriage_status!r},abo_blood_type={self.abo_blood_type!r},"
+            f"left_hand_flag={self.left_hand_flag!r},death_date={self.death_date!r},"
+            f"cellphone_number={self.cellphone_number!r},email={self.email!r},address={self.address!r},"
             f"gmt_create={self.gmt_create!r},gmt_modified={self.gmt_modified!r},is_deleted={self.is_deleted!r})"
         )
