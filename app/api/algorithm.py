3a265dc1efc59145b8a9e95a77fed55922e11771
蔡涛 <caitao@zhejianglab.com>
2023-01-13 10:30:51 +0800

    修复algorithm的bug

--- app/api/algorithm.py
+++ app/api/algorithm.py
@@ -1,32 +1,32 @@
+from typing import Any
+
 from fastapi import APIRouter, Depends
 
 from app.api.file import get_os_path
 from app.common.context import Context, researcher_context
 from app.common.depend import grpc_stub
-from app.common.util import protobuf_to_pydantic
-from app.db import crud
+from app.common.util import serialize_protobuf
+from app.db import common_crud
 from app.db.orm import File
 from app.grpc.eeg_pb2 import DisplayEEGRequest as GrpcDisplayEEGRequest
 from app.grpc.eeg_pb2_grpc import EEGServiceStub
 from app.model.request import DisplayEEGRequest
-from app.model.response import DisplayEEGData, Response
-from app.model.schema import FileInDB
+from app.model.response import Response, wrap_api_response
 
 router = APIRouter(tags=["algorithm"])
 
 
-@router.post("/api/displayEEG", description="查看EEG数据", response_model=Response[DisplayEEGData])
+@router.post("/api/displayEEG", description="查看EEG数据", response_model=Response[dict[str, Any]])
+@wrap_api_response
 def display_eeg(
     request: DisplayEEGRequest,
     ctx: Context = Depends(researcher_context),
     stub: EEGServiceStub = Depends(grpc_stub(EEGServiceStub)),
-) -> DisplayEEGData:
+) -> dict[str, Any]:
     grpc_request = GrpcDisplayEEGRequest(**request.dict())
-    file_in_db: FileInDB = crud.get_model(ctx.db, File, FileInDB, request.file_id)
-    grpc_request.file_path = get_os_path(
-        file_in_db.experiment_id, file_in_db.index, file_in_db.extension
-    )
-    grpc_request.file_type = file_in_db.extension
+    file = common_crud.select_row_by_id(ctx.db, File, request.file_id)
+    grpc_request.file_path = str(get_os_path(file.experiment_id, file.index, file.extension))
+    grpc_request.file_type = file.extension
     grpc_response = stub.displayEEG(grpc_request)
-    response = protobuf_to_pydantic(grpc_response, DisplayEEGData)
-    return response
+    response_dict = serialize_protobuf(grpc_response)
+    return response_dict
--- app/common/util.py
+++ app/common/util.py
@@ -2,9 +2,10 @@ import functools
 import sys
 from datetime import datetime, timezone, tzinfo
 from pathlib import Path
-from typing import Callable, MutableSequence, Type, TypeVar
+from typing import Sequence, TypeVar
 
 from dateutil import tz
+from google.protobuf.message import Message as GrpcMessage
 from pydantic import BaseModel
 
 from app.common.config import config
@@ -25,27 +26,19 @@ for sys_path in {Path(path).absolute() for path in sys.path}:
         SYS_PATHS.append(sys_path)
 
 
-def modify_model_field_by_type(model: Model, field_type: Type[T], map_func: Callable[[T], T]):
+def serialize_protobuf(model):
     if model is None:
         return None
-    field_schemas: dict[str, dict[str, str]] = model.schema(by_alias=False)["properties"]
-    for field_name in field_schemas.keys():
-        field_value = getattr(model, field_name)
-        if isinstance(field_value, field_type):
-            setattr(model, field_name, map_func(field_value))
-        if isinstance(field_value, BaseModel):
-            modify_model_field_by_type(field_value, field_type, map_func)
+    if isinstance(model, str):
+        return model
+    if isinstance(model, Sequence):
+        return [serialize_protobuf(item) for item in model]
+    if isinstance(model, GrpcMessage):
+        return {
+            field.name: serialize_protobuf(getattr(model, field.name))
+            for field in model.DESCRIPTOR.fields
+        }
     return model
-
-
-def protobuf_to_pydantic(model: T | None, target: type[Model]) -> Model | None:
-    if model is None:
-        return None
-    fields = {field.name: getattr(model, field.name) for field in model.DESCRIPTOR.fields}
-    for field_name, field_value in fields.items():
-        if isinstance(field_value, MutableSequence):
-            fields[field_name] = list(field_value)
-    return target(**fields)
 
 
 @functools.lru_cache(maxsize=None)
