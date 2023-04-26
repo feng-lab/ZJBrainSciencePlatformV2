e2e20ea426324f89e7870fe8d32e91cc4becb304
蔡涛 <caitao@zhejianglab.com>
2023-04-26 09:59:08 +0800

    添加数据迁移的脚本内容

--- alembic/sql/downgrade/5a840b6cfdfb_01b5380e17fc.sql
+++ alembic/sql/downgrade/5a840b6cfdfb_01b5380e17fc.sql
@@ -1,3 +1,33 @@
+USE zj_brain_science_platform;
+
+BEGIN;
+INSERT INTO file(id,
+                 experiment_id,
+                 paradigm_id,
+                 name,
+                 extension,
+                 size,
+                 is_original,
+                 gmt_create,
+                 gmt_modified,
+                 is_deleted)
+SELECT virtual_file.id,
+       virtual_file.experiment_id,
+       virtual_file.paradigm_id,
+       virtual_file.name,
+       virtual_file.file_type,
+       virtual_file.size,
+       virtual_file.is_original,
+       virtual_file.gmt_create,
+       virtual_file.gmt_modified,
+       FALSE
+FROM virtual_file
+         INNER JOIN storage_file ON virtual_file.id = storage_file.virtual_file_id
+WHERE virtual_file.is_deleted = FALSE
+  AND storage_file.is_deleted = FALSE
+  AND storage_file.name = virtual_file.name;
+COMMIT;
+
 SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE storage_file;
