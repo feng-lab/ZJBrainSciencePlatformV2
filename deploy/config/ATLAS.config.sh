2c0eb5cb3f497efbf73c54d2fd206bfd4b997282
蔡涛 <caitao@zhejianglab.com>
2023-09-14 16:22:26 +0800

    修改atlas配置

--- deploy/config/ATLAS.config.sh
+++ deploy/config/ATLAS.config.sh
@@ -1,4 +1,4 @@
 export PLATFORM_REPLICAS='1'
-export SSH_CONFIG_HOST='alivm1.zjlabserver'
-export SSH_IP='121.40.208.225'
-export SSH_USER='cnife'
+export SSH_CONFIG_HOST='alivm2.zjlabserver'
+export SSH_IP='8.210.254.154'
+export SSH_USER='ecs-user'
