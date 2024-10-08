199919e4074f6c3acd58996be63b85b0e5efd15c
xhli2 <1282372205@qq.com>
2024-10-08 09:54:39 +0800

    修改memo和c_therapyapi

--- app/common/config.py
+++ app/common/config.py
@@ -9,17 +9,17 @@ logger = logging.getLogger(__name__)
 
 class Config(BaseSettings):
     # 是否处于开发环境
-    DEBUG_MODE: bool = True
+    DEBUG_MODE: bool = False
 
     # 是否开启用户权限验证
-    ENABLE_AUTH: bool = False
+    ENABLE_AUTH: bool = True
 
     # 时区，默认
     TIMEZONE: str = "Asia/Shanghai"
 
     # 数据库URL
-    DATABASE_URL: str = "mysql+pymysql://zjlab:zjlab2022@localhost:8100/zj_brain_science_platform"
-    # DATABASE_URL: str = "mysql+pymysql://zjlab:J8ILKg0MnVRmgRwLsT4F@rm-bp127f6l8d5xs74264o.rwlb.rds.aliyuncs.com/zj_brain_science_platform"
+    # DATABASE_URL: str = "mysql+pymysql://zjlab:zjlab2022@localhost:8100/zj_brain_science_platform"
+    DATABASE_URL: str = "mysql+pymysql://zjlab:J8ILKg0MnVRmgRwLsT4F@rm-bp127f6l8d5xs74264o.rwlb.rds.aliyuncs.com/zj_brain_science_platform"
     # DATABASE_URL: str = "mysql+pymysql://zjlab:J8ILKg0MnVRmgRwLsT4F@rm-bp127f6l8d5xs7426.rwlb.rds.aliyuncs.com/zj_brain_science_platform"
 
     # 数据库配置，JSON格式
