95792be1e1b9e71ad314e969077f5f25e6c1a417
xhli2 <1282372205@qq.com>
2025-01-10 17:33:13 +0800

    gitlab workflow

--- test/__init__.py
+++ test/__init__.py
@@ -73,4 +73,3 @@ def login(username: str, password: str) -> dict[str, str]:
     token = ro.access_token
     assert token
     return {"Authorization": f"Bearer {token}"}
-http://gitlab.zhejianglab.com/research-center-for-life-sciences-computing/platform-infrastructure-group/lscp/zjbrainscience-front.git
